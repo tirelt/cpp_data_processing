f8332197d04d89fdf97dfb9f9131c7e6eb0a839b
Mac theo <theotirel@gmail.com>
2025-11-23 12:02:48 +0800

    trying eigen dense

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <Eigen/Dense>
+#include <chrono>
 
 int main(){
-    std::cout << "test" << std::endl;
+    using namespace Eigen;
+    using namespace std::chrono;
+
+    constexpr int N = 2000; // matrix size (2000x2000)
+
+    // Create two large random matrices
+    MatrixXd A = MatrixXd::Random(N, N);
+    MatrixXd B = MatrixXd::Random(N, N);
+
+    std::cout << "Starting heavy matrix multiplication of size " << N << "x" << N << "...\n";
+
+    auto start = high_resolution_clock::now();
+
+    MatrixXd C = A * B; // CPU heavy operation
+
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start).count();
+
+    std::cout << "Multiplication finished in " << duration << " ms\n";
+
+    // Print a checksum to verify correctness
+    std::cout << "Checksum: " << C.sum() << std::endl;
+
 }
